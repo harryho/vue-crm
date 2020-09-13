c1bcfc293d54892da8343f743a6139381aceb044
Cristiano Santana <cristianosantan>
2020-09-13 20:56:10 +0000

    comecei a fazer someproducts

---
+++ src/components/home/SomeProducts.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="cls-listProducts">
+        <ul id="example-1">
+            <li :for="item in this.dataProducts">
+                {{ item }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts" >
+    export default {
+        name: "SomeProducts",
+        props: { propProducts: Array },
+        
+        data() {
+            return {
+                dataProducts: Array
+            }
+        },
+
+        mounted() {
+            this.getProducts();
+        },
+
+        methods: {
+            getProducts() {
+                this.dataProducts = this.propProducts; 
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.cls-listProducts {
+    position: relative;
+}
+</style>
