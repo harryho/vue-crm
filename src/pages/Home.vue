5e29172a6e9c5e1f0ee965f0ff1d75297453a0fa
cristiano santana <lexcrt@gmail.com>
2020-10-04 16:11:05 -0300

    instalei o cordova

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -165,25 +165,25 @@
       items: [
         {
           color: '#2980B9',
-          src: 'https://lh3.googleusercontent.com/proxy/hrh8jrHHKNW8JyR8tDQoT9AzJ9DL-hgIf7Y6gqFFUhMnIAefroEHjDkwy6ph9x-1vDRcoYsbUpispMhnmht8aHPJ3maNlCBmeQCqU2XPlrd0vtdItwc',
+          src: '#',
           title: 'Produtos',
           subTitle: 'Produtos e serviços de machine learning inovadores em uma plataforma confiável.',
         },
         {
           color: '#58D68D',
-          src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTnbG1rbMnQkAJeqy8GG1JJIqDO3EAuEXDVxA&usqp=CAU',
+          src: '#',
           title: 'AI Platform',
           subTitle: 'A AI Platform, nosso ambiente de desenvolvimento de ciência de dados baseado em código, permite que desenvolvedores de ML e cientistas de dados assumam projetos.',
         },
         {
           color: '#82E0AA',
-          src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSvqQ7F3ZToOxhEFVjZE7Rb3XBd95vgR9LVSQ&usqp=CAU',
+          src: '#',
           title: 'IA facilitam',
           subTitle: 'Os elementos essenciais de IA facilitam a inclusão de visão, linguagem, conversa e dados estruturados nos seus aplicativos.',
         },
         {
           color: '#229954',
-          src: 'https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2018/03/02/plataforma-brasileira-jobecam-usa-ia-para-conectar-profissionais-a-empregadores.jpg',
+          src: '#',
           title: 'AI Hub',
           subTitle: 'O AI Hub, nosso repositório hospedado de componentes de IA prontos para usar, estimula a experimentação e a colaboração dentro da sua organização.',
         },
